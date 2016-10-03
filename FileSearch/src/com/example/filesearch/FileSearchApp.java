b378b2893bbe9cc840ed979f4c2027cf3858c45d
Matt Bajorek <bajorek.matthew@gmail.com>
2016-10-03 12:38:54 -0400

    Working Java 6 walk directory.

--- FileSearch/src/com/example/filesearch/FileSearchApp.java
+++ FileSearch/src/com/example/filesearch/FileSearchApp.java
@@ -1,6 +1,13 @@
 package com.example.filesearch;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 
 public class FileSearchApp {
 	String path;
@@ -14,12 +21,12 @@ public class FileSearchApp {
 			case 0:
 				System.out.println("USAGE: FileSearchApp path [regex] [zipfile]");
 				return;
-			case 1: app.setPath(args[0]);
-			case 2: app.setRegex(args[1]);
 			case 3: app.setZipFileName(args[2]);
+			case 2: app.setRegex(args[1]);	
+			case 1: app.setPath(args[0]);
 		}
 		try {
-			app.walkDirectory(app.getPath());
+			app.walkDirectoryJava6(app.getPath());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -37,6 +44,42 @@ public class FileSearchApp {
 	
 	public void addFileToZip(File file) {
 		System.out.println("addFileToZip: " + file);
+	}
+	
+	// File class
+	public void walkDirectoryJava6(String path) throws IOException {
+		File dir = new File(path);
+		File[] files = dir.listFiles();
+		
+		for (File file : files) {
+			if (file.isDirectory()) {
+				walkDirectoryJava6(file.getAbsolutePath());
+			} else {
+				processFile(file);
+			}
+		}
+	}
+	
+	// NIO class
+	public void walkDirectoryJava7(String path) throws IOException {
+		Files.walkFileTree(Paths.get(path), new SimpleFileVisitor<Path>() {
+			@Override
+			public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
+				throws IOException {
+					processFile(file.toFile());
+					return FileVisitResult.CONTINUE;
+				}
+		});
+	}
+	
+	// Streams
+	public void walkDirectoryJava8(String path) throws IOException {
+		Files.walk(Paths.get(path))
+			.forEach(f -> processFile(f.toFile()));
+	}
+	
+	public void processFile(File file) {
+		System.out.println("processFile: " + file);
 	}
 
 	public String getPath() {
